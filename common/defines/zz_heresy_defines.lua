8d3faf2dd12ead7cea08382cb0cbc88411eb799f
neothiamin <neothiamin@gmail.com>
2020-06-05 17:39:16 -0400

    1.3

---
+++ common/defines/zz_heresy_defines.lua
@@ -0,0 +1,1 @@
+NDefines.NDiplomacy.HRE_HERETIC_PENALTY = -0.005 -- Per heretic prince (halved by Peace of Westphalia) --Halved from 0p01 because heretic princes are a lot more common earlier in Heresy
