7bbcdc3e2fc3a91b1f2940d288c043fbce533444
neothiamin <neothiamin@gmail.com>
2021-12-02 09:33:54 -0500

    Further reduced HRE Heretic Penalty Increased MTTH for "Accusations of Greed" event Corrected some localization grammar

--- common/defines/zz_heresy_defines.lua
+++ common/defines/zz_heresy_defines.lua
@@ -1,2 +1,2 @@
-NDefines.NDiplomacy.HRE_HERETIC_PENALTY = -0.008 -- Per heretic prince (halved by Peace of Westphalia) --Reduced from 0p01 because heretic princes are a lot more common earlier in Heresy
+NDefines.NDiplomacy.HRE_HERETIC_PENALTY = -0.003 -- Per heretic prince (halved by Peace of Westphalia) --Reduced from 0p01 because heretic princes are a lot more common earlier in Heresy
 NDefines.NReligion.MAX_CHRISTIAN_RELIGIOUS_CENTERS = 3			-- BTAddOn changed from 3   Compensating for more provinces and more tags   Ive seen too many games with only 3 protestant and reformed countries because all CoR hit in the same area   Apparently doesnt work leaving it anyway --Heresy - changed back to 3 because of busted localization
